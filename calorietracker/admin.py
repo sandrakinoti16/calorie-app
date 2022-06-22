96dc5a9d6db1dcf293381859a23369cb2c509053
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-06-22 14:46:50 +0300

    forms

--- calorietracker/admin.py
+++ calorietracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Food, FoodCategory, FoodLog, Image, Weight
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Food)
+admin.site.register(FoodCategory)
+admin.site.register(FoodLog)
+admin.site.register(Image)
+admin.site.register(Weight)
